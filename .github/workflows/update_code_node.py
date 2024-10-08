448ebf83e02fb71751ada68bf857063389c0846e
Tabia Ye <tabiaye@gmail.com>
2024-10-07 21:05:06 -0700

    some changes to the nodes and actions

--- .github/workflows/update_code_node.py
+++ .github/workflows/update_code_node.py
@@ -1,12 +1,38 @@
 # requests library which is similar to JS fetch
 import requests
+# To work with the environment variables
+import os
+# To work with the files
+import sys
+
+TAKTILE_API_KEY = os.getenv("TAKTILE_API_KEY")
+TAKTILE_URL = "https://eu-central-1.taktile-org.decide.taktile.com"
+
+CODE_NODE_MAP = {
+  # File Name -> [flow_id, node_id]
+  "Multiply.py": ["flow_id_1", "code_node_id_1"], 
+  "Summarize.py": ["flow_id_2", "code_node_id_2"], 
+}
+
+# Fetch the file name passed as argument
+file_path = sys.argv[1]
+
+# Fetch the file content
+def get_code_content(file_path):
+    with open(file_path, 'r') as f:
+        return f.read()
+
 
 def update_taketile_code_node(flow_id, node_id, code_content): 
-  url = f"https://eu-central-1.taktile-org.decide.taktile.com/run/api/v1/flows/patch-decision-graph/sandbox/decide"
+  """
+  Updates the node code contents.
+  NOTE: Does not do any validation to see if there are meaningful differences in the contents
+  """
+  url = f"{TAKTILE_URL}/run/api/v1/flows/patch-decision-graph/sandbox/decide"
   headers = {
     # This should come from the environment variables, this should NOT be in plain text. 
     # THIS IS VERY VERY VERY VERY SENSITIVE
-    "X-Api-Key": "c179117d-a4ce-4635-a268-b37d13668515",
+    "X-Api-Key": TAKTILE_API_KEY,
     "accept": "application/json",
     "Content-Type": "application/json"
   }
@@ -15,16 +41,17 @@ def update_taketile_code_node(flow_id, node_id, code_content):
     "node_id": node_id, 
     "src_code": code_content 
   }
-  response = requests.put(url, headers=headers, json=data)
+  response = requests.post(url, headers=headers, json=data)
   if response.status_code == 200:
     print(f"Succesfully updated Code Node {node_id}")
   else: 
     print(f"Failed to update Code Node {node_id}. Error: {response.text}")
 
-code_node_map = {
-  "Multiply.py": "code_node_id_1", 
-  "Summarize.py": "code_node_id_2"
-}
 
-
+if file_path in CODE_NODE_MAP:
+    code_content = get_code_content(file_path)
+    flow_id, node_id = CODE_NODE_MAP[file_path]
+    update_taketile_code_node(flow_id, node_id, code_content)
+else:
+    print(f"File {file_path} does not map to a known Code Node")
 
