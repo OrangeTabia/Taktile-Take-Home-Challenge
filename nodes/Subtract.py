aee10c7bf08f0e03b9a7e9dd154a9c8baf9ac989
Tabia Ye <tabiaye@gmail.com>
2024-10-07 21:12:33 -0700

    adding and changing a function

--- nodes/Subtract.py
+++ nodes/Subtract.py
@@ -1,10 +1,10 @@
-def multiply(data):
+def subtract(data):
     """Function that summarized cols a and b"""
 
-    data["sum"] = data["a"] * data["b"]
+    data["difference"] = data["a"] - data["b"]
 
     return data
 
 
 if __env:  # indicates we are running on Taktile
-    data = multiply(data)
+    data = subtract(data)
