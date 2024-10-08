7bf38e90b963b4f7df90e30abd4d7c8a2bcc3445
Tabia Ye <tabiaye@gmail.com>
2024-10-07 20:49:45 -0700

    Small Changes to the code node
    Include the flow_id

--- .github/workflows/update_code_node.py
+++ .github/workflows/update_code_node.py
@@ -1,17 +1,19 @@
 # requests library which is similar to JS fetch
 import requests
 
-def update_taketile_code_node(node_id, code_content): 
+def update_taketile_code_node(flow_id, node_id, code_content): 
   url = f"https://eu-central-1.taktile-org.decide.taktile.com/run/api/v1/flows/patch-decision-graph/sandbox/decide"
   headers = {
+    # This should come from the environment variables, this should NOT be in plain text. 
+    # THIS IS VERY VERY VERY VERY SENSITIVE
     "X-Api-Key": "c179117d-a4ce-4635-a268-b37d13668515",
     "accept": "application/json",
     "Content-Type": "application/json"
   }
   data = {
-    "flow_id": , 
-    "node_id": , 
-    "src_code" : 
+    "flow_id": flow_id, 
+    "node_id": node_id, 
+    "src_code": code_content 
   }
   response = requests.put(url, headers=headers, json=data)
   if response.status_code == 200:
